714a820cac06022fc85e8c96159de2d6cb330942
Doudi05 <gggg4444.wa@hotmail.com>
2023-03-19 21:03:19 +0100

    commit3

--- version.sh
+++ version.sh
@@ -1,7 +1,7 @@
 #!/bin/dash   
 
 # projet version.sh 
-# Akel Ward , Aiach Rami , TD2/TP2B
+# AKEL Ward , AIACH Rami , TD2/TP2B
 
 # Permet l'affichage de la commande help quand on en a besoin 
 # pas de paramètres attendu
@@ -41,7 +41,22 @@ usage (){
 
 # affiche l'aide
 help (){
-    echo "Entrer ./version.sh --help for more information." >&2
+    echo "Enter ./version.sh --help for more information." >&2
+}
+
+# Fonction qui vérifie qu'un argument n’est pas vide, et qu’il est bien sur une seule ligne.
+# Si ce n’est pas le cas, affiche un message d’erreur et quitte le programme.
+# $1 : le nom de l’argument
+checkArgument (){
+    if [ -z "$1" ] ; then
+        echo "Erreur : l'argument $1 est vide" >&2
+        exit 1
+    fi
+
+    if [ $(echo "$1" | wc -l) -ne 1 ] ; then
+        echo "Erreur : l'argument $1 contient plusieurs lignes" >&2
+        exit 1
+    fi
 }
 
 
@@ -51,15 +66,13 @@ help (){
 # Le fichier doit être ajouté dans le répertoire .versioning du répertoire courant.
 
 addCommande (){
-    # Vérifier les arguments
-    if [ $# -ne 2 ]; then
-        echo "Usage : $0 add FILE MESSAGE"
-        exit 1
-    fi
+    # Écrire le message de log associé à la première version
+    checkArgument "$2"
 
-    # Vérifier si le fichier existe
-    if [ ! -f "$1" ]; then
-        echo "Erreur : $1 n'est pas un fichier valide"
+    # vérifier que le fichier FILE est un fichier ordinaire existant avec la permission de lecture
+    if [ ! -f "$1" ] && [ ! -r "$1" ]; then
+        echo "Error! $1 is not a regular file or read permission is not granted."
+        help
         exit 1
     fi
 
@@ -68,18 +81,19 @@ addCommande (){
         mkdir .versioning
     fi
 
-    # Copier le fichier dans le répertoire .versioning
-    cp "$1" ".versioning/$1.v1"
-
-    # Écrire le message de log associé à la première version
-    if [ -z "$2" || "$2" != "$(echo "$2" | tr -d '\n')" ]; then
-        echo "Erreur : le commentaire est vide ou contient des sauts de ligne"
+    # si le fichier existe déjà dans le répertoire .versioning, on renvoie une erreur
+    if [ -f ".versioning/$1.1" ]; then
+        echo "Error! $1 is already under versioning."
+        help
         exit 1
     fi
+
+    #Copier le fichier dans le répertoire .versioning
+    cp "$1" ".versioning/$1.1"
     
     echo "$(date -R) '$(echo "$2" | tr -d '[:space:]')'" >> ".versioning/$1.log"
 
-    echo "Le fichier $1 a été ajouté sous versioning avec succès"
+    echo "Added a new file under versioning: '$1'"
 }
 
 
@@ -87,36 +101,30 @@ addCommande (){
 # FILE est le nom du fichier à supprimer sous versioning.
 
 rmCommande (){
-    # Vérifier les arguments
-    if [ $# -ne 1 ]; then
-        echo "Usage : $0 rm FILE"
-        exit 1
-    fi
-
     # Vérifier si le fichier existe dans le répertoire .versioning
-    if [ ! -f ".versioning/$1.v1" ]; then
-        echo "Erreur : $1 n'est pas sous versioning"
+    if [ ! -f ".versioning/$1.1" ]; then
+        echo "Error! $1 is not under versioning."
         exit 1
     fi
 
     # Demander une confirmation à l'utilisateur
-    echo "Êtes-vous sûr de vouloir supprimer toutes les versions de $1 ? (y/n)"
+    echo "Are you sure you want to delete $1 from versioning ? (yes/no)"
     read confirmation
 
     # Vérifier la sensibilité à la casse de la réponse
-    if [ "$confirmation" != "y" || "$confirmation" != "Y" ]; then
-        echo "Suppression annulée"
+    if [ "$confirmation" != "yes" ] && [ "$confirmation" != "YES" ]; then
+        echo "Nothing done."
         exit 0
     else
         # Supprimer toutes les versions du fichier
         rm .versioning/$1.*
 
-        echo "Toutes les versions de $1 ont été supprimées avec succès"
+        echo "$1 is not under versioning anymore."
 
         # Vérifier si le répertoire .versioning est vide et le supprimer le cas échéant
         if [ -z "$(ls -A .versioning)" ]; then
             rm -r .versioning
-            echo "Le répertoire .versioning a été supprimé avec succès"
+            echo "The .versioning directory was successfully deleted" 
         fi
     fi
 }
@@ -127,41 +135,31 @@ rmCommande (){
 # MESSAGE est le message de log associé à la nouvelle version du fichier.
 
 commitCommande (){
-    # Vérifier les arguments
-    if [ $# -ne 2 ]; then
-        echo "Usage : $0 commit|ci FILE MESSAGE"
-        exit 1
-    fi
+    # Écrire le message de log associé à la première version
+    checkArgument "$2"
 
     # Vérifier si le fichier existe dans le répertoire .versioning
-    if [ ! -f ".versioning/$1.v1" ]; then
-        echo "Erreur : $1 n'est pas sous versioning"
+    if [ ! -f ".versioning/$1.1" ]; then
+        echo "Error! $1 is not under versioning."
         exit 1
     fi
 
     # Vérifier si le fichier courant est identique à la dernière version
-    if cmp -s "$1" ".versioning/$1.v1"; then
-        echo "Le fichier $1 n'a pas été modifié"
-        exit 0
-    fi
+    last_version=$(ls -1 .versioning | grep "$1." | cut -d '.' -f 3 | sort -n | tail -1)
 
-    # Vérifier le numéro de la dernière version
-    last_version=$(ls -1 .versioning | grep "$1.v" | sort -n | tail -1 | cut -d'.' -f3 | cut -d'v' -f2)
-    
-    echo "Le numéro de la dernière version est : $last_version"
-
-    # Sauvegarder la nouvelle version
-    cp "$1" ".versioning/$1.v$((last_version + 1))"
-
-    # Écrire le message de log associé à la nouvelle version
-    if [ -z "$2" || "$2" != "$(echo "$2" | tr -d '\n')" ]; then
-        echo "Erreur : le commentaire est vide ou contient des sauts de ligne"
+    if $(cmp -s "$1" ".versioning/$1.$last_version"); then
+        echo "Error! $1 is already up to date, nothing to commit."
         exit 1
     fi
+
+    echo "Committed a new version: $(($last_version + 1))"
+
+    # Sauvegarder la nouvelle version
+    cp "$1" ".versioning/$1.$((last_version + 1))"
     
     echo "$(date -R) '$(echo "$2" | tr -d '[:space:]')'" >> ".versioning/$1.log"
 
-    echo "La nouvelle version du fichier $1 a été sauvegardée avec succès"
+    echo "The new version of $1 has been committed successfully."
 }
 
 
@@ -169,29 +167,23 @@ commitCommande (){
 # FILE est le nom du fichier à comparer.
 
 diffCommande (){
-    # Vérifier les arguments
-    if [ $# -ne 1 ]; then
-        echo "Usage : $0 diff FILE"
-        exit 1
-    fi
-
     # Vérifier si le fichier existe dans le répertoire .versioning
-    if [ ! -f ".versioning/$1.v1" ]; then
-        echo "Erreur : $1 n'est pas sous versioning"
+    if [ ! -f ".versioning/$1.1" ]; then
+        echo "Error! $1 is not under versioning."
         exit 1
     fi
 
     # Vérifier le numéro de la dernière version
-    last_version=$(ls -1 .versioning | grep "$1.v" | sort -n | tail -1 | cut -d'.' -f3 | cut -d'v' -f2)
+    last_version=$(ls -1 .versioning | grep "$1." | cut -d '.' -f 3 | sort -n | tail -1)
 
     # Vérifier si le fichier courant est identique à la dernière version
-    if cmp -s "$1" ".versioning/$1.v$last_version"; then
-        echo "Le fichier $1 n'a pas été modifié"
-        echo "Il n'y a pas de différence entre la dernière version et le fichier courant"
+    if $(cmp -s "$1" ".versioning/$1.$last_version"); then
+        echo "The file $1 has not been modified since the last commit."
+        echo "No difference between the current version and the last one."
         exit 0
     else
         # Afficher la différence entre le fichier courant et la dernière version
-        diff "$1" ".versioning/$1.v$last_version"
+        diff "$1" ".versioning/$1.$last_version"
     fi
 }
 
@@ -201,25 +193,19 @@ diffCommande (){
 # NUMBER est le numéro de la version à restaurer.
 
 checkoutCommande (){
-    # Vérifier les arguments
-    if [ $# -ne 1 ] && [ $# -ne 2 ]; then
-        echo "Usage : $0 checkout|co FILE [NUMBER]"
-        exit 1
-    fi
-
     # Vérifier si le fichier existe dans le répertoire .versioning
-    if [ ! -f ".versioning/$1.v1" ]; then
-        echo "Erreur : $1 n'est pas sous versioning"
+    if [ ! -f ".versioning/$1.1" ]; then
+        echo "Error! $1 is not under versioning."
         exit 1
     fi
 
     # Vérifier le numéro de la dernière version
-    last_version=$(ls -1 .versioning | grep "$1.v" | sort -n | tail -1 | cut -d'.' -f3 | cut -d'v' -f2)
+    last_version=$(ls -1 .versioning | grep "$1." | cut -d '.' -f 3 | sort -n | tail -1)
 
     # Vérifier si le fichier courant est identique à la dernière version
-    if cmp -s "$1" ".versioning/$1.v$last_version"; then
-        echo "Le fichier $1 n'a pas été modifié"
-        echo "Il n'y a pas de différence entre la dernière version et le fichier courant"
+    if $(cmp -s "$1" ".versioning/$1.$last_version"); then
+        echo "The file $1 has not been modified since the last commit."
+        echo "No difference between the current version and the last one."
         exit 0
     fi
 
@@ -227,17 +213,17 @@ checkoutCommande (){
     if [ $# -eq 2 ]; then
         # Vérifier si le numéro de version est valide
         if [ $2 -lt 1 ] || [ $2 -gt $last_version ]; then
-            echo "Erreur : le numéro de version doit être compris entre 1 et $last_version"
+            echo "Error! the version number must be between 1 and $last_version"
             exit 1
         fi
 
         # Restaurer la version spécifiée
-        cp ".versioning/$1.v$2" "$1"
-        echo "La version $2 du fichier $1 a été restaurée avec succès"
+        cp ".versioning/$1.$2" "$1"
+        echo "The version $2 of the file $1 has been restored successfully"
     else
         # Restaurer la dernière version
-        cp ".versioning/$1.v$last_version" "$1"
-        echo "La dernière version du fichier $1 a été restaurée avec succès"
+        cp ".versioning/$1.$last_version" "$1"
+        echo "The last version of the file $1 has been restored successfully"
     fi
 }
 
@@ -246,15 +232,9 @@ checkoutCommande (){
 # FILE est le nom du fichier dont on veut afficher les logs.
 
 logCommande (){
-    # Vérifier les arguments
-    if [ $# -ne 1 ]; then
-        echo "Usage : $0 log FILE"
-        exit 1
-    fi
-
     # Vérifier si le fichier existe dans le répertoire .versioning
-    if [ ! -f ".versioning/$1.v1" ]; then
-        echo "Erreur : $1 n'est pas sous versioning"
+    if [ ! -f ".versioning/$1.1" ]; then
+        echo "Error! $1 is not under versioning."
         exit 1
     fi
     
@@ -268,25 +248,19 @@ logCommande (){
 # NUMBER est le numéro de la version à restaurer.
 
 resetCommande (){
-    # Vérifier les arguments
-    if [ $# -ne 2 ]; then
-        echo "Usage : $0 reset FILE [NUMBER]"
-        exit 1
-    fi
-
     # Vérifier si le fichier existe dans le répertoire .versioning
-    if [ ! -f ".versioning/$1.v1" ]; then
-        echo "Erreur : $1 n'est pas sous versioning"
+    if [ ! -f ".versioning/$1.1" ]; then
+        echo "Error! $1 is not under versioning."
         exit 1
     fi
 
     # Vérifier le numéro de la dernière version
-    last_version=$(ls -1 .versioning | grep "$1.v" | sort -n | tail -1 | cut -d'.' -f3 | cut -d'v' -f2)
+    last_version=$(ls -1 .versioning | grep "$1." | cut -d '.' -f 3 | sort -n | tail -1)
 
     # Vérifier si le fichier courant est identique à la dernière version
-    if cmp -s "$1" ".versioning/$1.v$last_version"; then
-        echo "Le fichier $1 n'a pas été modifié"
-        echo "Il n'y a pas de différence entre la dernière version et le fichier courant"
+    if $(cmp -s "$1" ".versioning/$1.$last_version"); then
+        echo "The file $1 has not been modified since the last commit."
+        echo "No difference between the current version and the last one."
         exit 0
     fi
     
@@ -294,25 +268,38 @@ resetCommande (){
     if [ $# -eq 2 ]; then
         # Vérifier si le numéro de version est valide
         if [ $2 -lt 1 ] || [ $2 -gt $last_version ]; then
-            echo "Erreur : le numéro de version doit être compris entre 1 et $last_version"
+            echo "Error! the version number must be between 1 and $last_version"
+            exit 1
+        elif [ $2 -eq $last_version ]; then
+            echo "Checked out to the latest version, nothing to reset."
             exit 1
         fi
 
-        # Restaurer la version spécifiée
-        cp ".versioning/$1.v$2" "$1"
-        echo "La version $2 du fichier $1 a été restaurée avec succès"
-        
-        # Supprimer les versions du numéro strictement supérieur au numéro de version spécifié.
-        # les logs des versions supprimées sont également supprimés.
-        while [ $last_version -gt $2 ]; do
-            rm ".versioning/$1.v$last_version"
-            sed -i "$last_version d" ".versioning/$1.log"
-            last_version=$((last_version-1))
-        done
+        # Demander une confirmation à l'utilisateur
+        echo "Are you sure you want to reset the file $1 to the version $2? (yes/no)"
+        read confirmation
+
+        # Vérifier la sensibilité à la casse de la réponse
+        if [ "$confirmation" != "yes" ] && [ "$confirmation" != "YES" ]; then
+            echo "Nothing done."
+            exit 0
+        else
+            # Restaurer la version spécifiée
+            cp ".versioning/$1.$2" "$1"
+            echo "The version $2 of the file $1 has been restored successfully"
+            
+            # Supprimer les versions du numéro strictement supérieur au numéro de version spécifié.
+            # les logs des versions supprimées sont également supprimés.
+            while [ $last_version -gt $2 ]; do
+                rm ".versioning/$1.$last_version"
+                sed -i "$last_version d" ".versioning/$1.log"
+                last_version=$((last_version-1))
+            done
+        fi
     else
         # Restaurer la dernière version
-        cp ".versioning/$1.v$last_version" "$1"
-        echo "La dernière version du fichier $1 a été restaurée avec succès"
+        cp ".versioning/$1.$last_version" "$1"
+        echo "The last version of the file $1 has been restored successfully"
     fi
 }
 
@@ -322,23 +309,20 @@ resetCommande (){
 # MESSAGE est le message de log de la version.
 
 amendCommande (){
-    # Vérifier les arguments
-    if [ $# -lt 1 ] || [ $# -gt 2 ]; then
-        echo "Usage : $0 amend FILE [MESSAGE]"
-        exit 1
-    fi
+    # Écrire le message de log associé à la première version
+    checkArgument "$2"
 
     # Vérifier si le fichier existe dans le répertoire .versioning
-    if [ ! -f ".versioning/$1.v1" ]; then
+    if [ ! -f ".versioning/$1.1" ]; then
         echo "Erreur : $1 n'est pas sous versioning"
         exit 1
     fi
 
     # Vérifier le numéro de la dernière version
-    last_version=$(ls -1 .versioning | grep "$1.v" | sort -n | tail -1 | cut -d'.' -f3 | cut -d'v' -f2)
+    last_version=$(ls -1 .versioning | grep "$1." | cut -d '.' -f 3 | sort -n | tail -1)
 
     # Vérifier si le fichier courant est identique à la dernière version
-    if cmp -s "$1" ".versioning/$1.v$last_version"; then
+    if $(cmp -s "$1" ".versioning/$1.$last_version"); then
         echo "Le fichier $1 n'a pas été modifié"
         echo "Il n'y a pas de différence entre la dernière version et le fichier courant"
         exit 0
@@ -346,20 +330,17 @@ amendCommande (){
 
     # Vérifier si le message de log est spécifié
     if [ $# -eq 2 ]; then
-        # Modifier le message de log de la dernière version
-        sed -i "$last_version s/.*/$2/" ".versioning/$1.log"
-        echo "Le message de log de la version $last_version du fichier $1 a été modifié avec succès"
+        # Modifier le message de log de la dernière version en gardant la date de la dernière version
+        dates="$(date -R) '$(echo "$2" | tr -d '[:space:]')'" >> ".versioning/$1.log"
+        sed -i "$last_version s/.*/$dates/" ".versioning/$1.log"
+        echo "The log message of the last version of the file $1 has been modified successfully"
     fi
 
     # Restaurer la dernière version
-    cp "$1" ".versioning/$1.v$last_version"
-    echo "La dernière version du fichier $1 a été restaurée avec succès"
+    cp "$1" ".versioning/$1.$last_version"
+    echo "The last version of the file $1 has been restored successfully"
+    echo "Last version amended: $last_version"
 }
-
-
-
-
-
 
 
 
@@ -371,41 +352,94 @@ if test $# -eq 1 && test $1 = "--help" ; then
     usage
     exit 0
 elif test $# -eq 0 ; then
-    echo "Erreur commande"
     help
     exit 1
 else
     case $1 in
         add)
-            addCommande $2 $3
+            # Vérifier les arguments
+            if [ $# -ne 3 ]; then
+                echo "Usage : $0 add FILE MESSAGE"
+                help
+                exit 1
+            else
+                addCommande $2 $3
+            fi
             ;;
         rm)
-            rmCommande $2
+            # Vérifier les arguments
+            if [ $# -ne 2 ]; then
+                echo "Usage : $0 rm FILE"
+                help
+                exit 1
+            else
+                rmCommande $2
+            fi
             ;;
         commit|ci)
-            commitCommande $2 $3
+            # Vérifier les arguments
+            if [ $# -ne 3 ]; then
+                echo "Usage : $0 commit FILE MESSAGE"
+                help
+                exit 1
+            else
+                commitCommande $2 $3
+            fi
             ;;
         diff)
-            diffCommande $2
+            # Vérifier les arguments
+            if [ $# -ne 2 ]; then
+                echo "Usage : $0 diff FILE"
+                help
+                exit 1
+            else
+                diffCommande $2
+            fi
             ;;
         checkout|co)
-            checkoutCommande $2 $3
+            # Vérifier les arguments
+            if [ $# -lt 2 ] || [ $# -gt 3 ]; then
+                echo "Usage : $0 checkout FILE [NUMBER]"
+                help
+                exit 1
+            else
+                checkoutCommande $2 $3
+            fi
             ;;
         log)
-            logCommande $2
+            # Vérifier les arguments
+            if [ $# -ne 2 ]; then
+                echo "Usage : $0 log FILE"
+                help
+                exit 1
+            else
+                logCommande $2
+            fi
             ;;
         reset)
-            resetCommande $2 $3
+            # Vérifier les arguments
+            if [ $# -ne 3 ]; then
+                echo "Usage : $0 reset FILE [NUMBER]"
+                help
+                exit 1
+            else
+                resetCommande $2 $3
+            fi
             ;;
         amend)
-            amendCommande $2 $3
+            # Vérifier les arguments
+            if [ $# -ne 3 ]; then
+                echo "Usage : $0 amend FILE [MESSAGE]"
+                help
+                exit 1
+            else
+                amendCommande $2 $3
+            fi
             ;;
         *)
-            echo "Erreur commande"
+            echo "Error! This command name does not exist: $1"
             help
             exit 1
             ;;
     esac
 fi
-
-# Peut-on ajouter des dossiers? à voir
